24b86fa02a8e01b742723c5ac028713bf786cf6d
motoyama-daishi <48724872dad@gmail.com>
2023-02-04 11:16:54 +0900

    add: カウンター機能

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,37 @@
 <script lang="ts">
-  let count: number = 0
-  const increment = () => {
-    count += 1
+  export let currentCount: number = 0
+  export let onClickIncrement: () => void
+  export let onClickDecrement: () => void
+  export let onClickClear: (value: number) => void
+
+  const handleOnClickIncrement = () => {
+    onClickIncrement?.()
+  }
+  const handleOnClickDecrement = () => {
+    if(currentCount === 0) return
+    onClickDecrement?.()
+  }
+  const handleOnClickClear = () => {
+    onClickClear?.(currentCount)
   }
 </script>
 
-<button on:click={increment}>
-  count is {count}
-</button>
+<div class={'button-group'}>
+  <p>{currentCount}</p>
+  <button on:click={handleOnClickIncrement}>
+    +
+  </button>
+  <button on:click={handleOnClickDecrement}>
+    -
+  </button>
+  <button on:click={handleOnClickClear}>
+    クリア
+  </button>
+</div>
+
+<style>
+  div.button-group {
+    display: flex;
+    gap: 4px;
+  }
+</style>
